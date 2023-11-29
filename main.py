70c0fcd4540d578863e3dcad3ec808eb509ecbe9
jrw2936 <jrw2936@g.rit.edu>
2023-11-29 15:16:59 -0500

    did some more work

--- main.py
+++ main.py
@@ -2,18 +2,44 @@ import numpy as np
 import open3d as o3d
 import os
 
+def background_subtract(curr, prev):
+    min_distance = 0.001
+    if prev is not None:
+        distances = curr.compute_point_cloud_distance(prev)
+        distances = np.asarray(distances)
+        ind = np.where(distances > min_distance)[0]
+        return curr.select_by_index(ind)
+    return curr
+
+
 def main():
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    ground_threshold = 0.2
     height_threshold = 3
+    min_points = 2
+    clustering_epsilon = 2.2
     directory_path = os.path.join(os.getcwd(), 'dataset', 'PointClouds')
-    for filename in os.listdir(os.path.join(os.getcwd(), r'dataset\PointClouds')):
-        file_path = os.path.join(directory_path, filename)
+    prev = None
+    for i in range(500):
+        file_path = os.path.join(directory_path, f'{i}.pcd')
         pointcloud = o3d.io.read_point_cloud(file_path)
-        _, inliers = pointcloud.segment_plane(distance_threshold=0.5, ransac_n=3, num_iterations=1000)
-        pointcloud = pointcloud.select_by_index(inliers, invert=True)
-        points = np.asarray(pointcloud.points)
+        # First handle background subtraction
+        result = background_subtract(pointcloud, prev)
+        # Then, use height thresholding to get rid of any points not belonging to cars
+        points = np.asarray(result.points)
         filtered_points = points[points[:, 2] <= height_threshold]
+        filtered_points = filtered_points[filtered_points[:, 2] >= ground_threshold]
+        prev = pointcloud
         filtered_pcd = o3d.geometry.PointCloud()
         filtered_pcd.points = o3d.utility.Vector3dVector(filtered_points)
-        o3d.visualization.draw_geometries([filtered_pcd])
+        labels = np.array(filtered_pcd.cluster_dbscan(clustering_epsilon, min_points))
+        max_label = labels.max()
+        print(f"{i} has {max_label + 1} clusters")
+        if prev is not None: vis.remove_geometry(prev)
+        vis.poll_events()
+        vis.update_renderer()
+        vis.add_geometry(filtered_pcd)
+    
 if __name__ == '__main__':
     main()
