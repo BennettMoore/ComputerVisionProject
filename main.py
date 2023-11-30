487690bd343e823fd168914cd8924c05803d8c42
Bennett Moore <bwm7637@rit.edu>
2023-11-29 23:03:24 -0500

    csvs now record data and are stored properly, and added option to show all points in the point cloud

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 import open3d as o3d
 import os
 
+# TODO Set me to False before turning in
+SHOW_EVERYTHING = True # Debugging feature to help give each cluster more context in the total point cloud
 
 # A set of distinct colors to distinguish different clusters
 CLUSTER_COLORS = np.array([
-    [0.75,0.75,0.75], # Gray
     [1.,0.,0.], # Red
     [0.,1.,0.], # Green
     [0.,0.,1.], # Blue
@@ -20,6 +21,7 @@ CLUSTER_COLORS = np.array([
     [0.,1.,0.75], # Turquoise
     [1.,0.5,0.75], # Pink
     [0.5,0.5,1.], # Lavender
+    [0.75,0.75,0.75], # Gray
 ])
 
 def background_subtract(curr, prev):
@@ -75,6 +77,14 @@ def main():
         max_label = labels.max()
         # print(labels)
         filtered_pcd.colors = o3d.utility.Vector3dVector(color_clusters(labels))
+        if SHOW_EVERYTHING:
+            total_points = np.concatenate((np.asarray(filtered_pcd.points), np.asarray(pointcloud.points)), axis=0)
+            total_colors = np.concatenate((np.asarray(filtered_pcd.colors), np.zeros((np.asarray(pointcloud.points).shape[0], 3), dtype=np.float64)), axis=0)
+            total_pcd = o3d.geometry.PointCloud()
+            total_pcd.points = o3d.utility.Vector3dVector(total_points)
+            total_pcd.colors = o3d.utility.Vector3dVector(total_colors)
+        
+        
         # Debug statement to view number of clusters
         print(f"Frame {i} has {max_label + 1} clusters")
 
@@ -88,13 +98,13 @@ def main():
         # Include this for now, so that we start processing when we have the first stable frame (if he grades it with another set of pointclouds)
         if max_label + 1 == num_cars and stable_flag is False:
             stable_flag = True
-            with open(f'frame_{i}.csv', 'w', newline='') as csv_file:
+            with open(f'perception_results/frame_{i}.csv', 'w', newline='') as csv_file:
                 csv_writer = csv.writer(csv_file)
                 csv_writer.writerow(file_header)
-                for j in range(num_cars):
+                for j in range(num_cars-1, -1, -1):
                     this_cluster = []
                     for k in range(len(labels)):
-                        if j == labels[k]+1:
+                        if j == labels[k]:
                             this_cluster.append([filtered_points[k][0], filtered_points[k][1], filtered_points[k][2]])
                     num_points = len(this_cluster)
                     x_sum = sum(point[0] for point in this_cluster)
@@ -102,13 +112,15 @@ def main():
                     z_sum = sum(point[2] for point in this_cluster)
 
                     midpoint = (x_sum / num_points, y_sum / num_points, z_sum / num_points)
+                    curr_id = starting_id + j
+                    csv_writer.writerow([curr_id, midpoint[0], midpoint[1], midpoint[2], 0, 0, 0, 0, 0, 0, 0, 0, 0])
                     clusters.append(this_cluster)
                     midpoints.append(midpoint)
                 midpoints = sorted(midpoints, key=lambda point: point[0])
                 # print(midpoints)
-                curr_id = starting_id
-                # csv_writer.writerow()
-        o3d.visualization.draw_geometries([filtered_pcd])
+            
+        if SHOW_EVERYTHING: o3d.visualization.draw_geometries([total_pcd])
+        else: o3d.visualization.draw_geometries([filtered_pcd])
     
 if __name__ == '__main__':
     main()
